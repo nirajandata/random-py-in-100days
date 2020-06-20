6d30007930c5f0e984286a776c43c56b84e0851a
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2020-06-20 20:33:56 +0545

    Update random_turtle.py

--- random_turtle.py
+++ random_turtle.py
@@ -1,20 +1,19 @@
 import turtle
 def _draw():
 	window=turtle.Screen()
-	
+	window.bgcolor("indigo")
  	a=turtle.Turtle()
 	i=0
 	j=-20
-	x=['grey','black','blue','green','red','white','yellow','white']
-	while(i<7):
+	x=['grey','black','blue','green','red','white','yellow','white','green','red']
+	while(i<len(x)):
 	 a.circle(20+j,10+j)
-	 j=j+40
+	 j=j+30
+	 
+	 # window.bgcolor(x[i-1]) // don;t uncomment it 
+	 #a.fill(x[i]) # uncomment this and see strange phenomenon :) , well not all strange though 
+	 a.color(x[i]) # using a.color(x[i-1]) draws violently , try it !
 	 i=i+1
-	 window.bgcolor(x[i-1])
-	 if (i==7):
-	  i=0
-	 # a.fill(x[i]) # uncomment this and see strange phenomenon :) 
-	 a.color(x[i])
 _draw()
 
 input()
