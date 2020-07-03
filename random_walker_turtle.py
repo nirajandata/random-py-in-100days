971ebf0d106e03e9e036a3762f75590b7b541958
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2020-07-03 16:00:58 +0545

    Update random_walker_turtle.py

--- random_walker_turtle.py
+++ random_walker_turtle.py
@@ -1,6 +1,6 @@
 import turtle as t, random
-for _ in range(80):
-    ran=random.random()*10
+for _ in range(800):
+    ran=random.random()*100
     a=random.choice([-ran,ran])
     random.choice([t.forward,t.backward])(a)
     b=random.choice([90,-90])
